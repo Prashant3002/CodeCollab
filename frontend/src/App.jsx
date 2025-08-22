592a8d11ebf175fe770f00f1c60e8ba65a919550
Pragathi <kodmadpragathi@gmail.com>
2025-08-22 22:41:24 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './Components/LandingPage';
-import LoginPage from './Components/LoginPage';
-import SignupPage from './Components/SignupPage';
+// import LoginPage from './Components/LoginPage';
+// import SignupPage from './Components/SignupPage';
 import DashboardPage from './Components/DashboardPage';
 import ProjectsPage from './Components/ProjectsPage'
 import AnalyticsPage from './Components/AnalyticsPage'
 import ProfilePage from './Components/ProfilePage'
-import SettingsPage from './Components/SettingsPage'
+import SettingPage from './Components/SettingPage'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 // Import Bootstrap JS
@@ -18,13 +18,13 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        {/* <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} /> */}
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/analytics" element={<AnalyticsPage />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/setting" element={<SettingPage />} />
       </Routes>
     </Router>
   )
